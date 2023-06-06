76e4c09efdcaa62d3570a8380f5aca1417cef4ff
Jibson Oliveira Filho <cilvioster@gmail.com>
2023-06-06 15:10:46 -0300

    deleção arquivos

--- bot.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-mensagem = { "text": "Olá, bem-vindo ao canal de testes"}
-webhook = "https://hooks.slack.com/services/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"
-
-# Postar mensagens no canal
-requests.post(webhook, data=json.dumps(mensagem))
